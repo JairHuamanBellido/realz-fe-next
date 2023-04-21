66ca5cc649360cad399f197497944484ed809e31
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2023-04-21 17:27:32 -0500

    feat: add middleware for guard lobby and chat pages

---
+++ pages/auth.tsx
@@ -0,0 +1,17 @@
+import { signIn } from "next-auth/react";
+import Head from "next/head";
+
+export default function AuthPage() {
+  return (
+    <>
+      <Head>
+        <title>Realz | Authentication</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Authentication</h1>
+      </main>
+    </>
+  );
+}
