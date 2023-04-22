0ae1c2f55ad389e39d4c9dc3a3c93c28caf8f484
Jair Huaman Bellido <huamanbellidoj@gmail.com>
2023-04-22 09:05:16 -0500

    feat: add validation to show create room button for github users (#19)
    * feat: add validation to show create room button for github users
    
    * fix: update cookie time to 1 day

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,34 +5,55 @@ import { CreateUserUseCase } from "@/src/domain/user/use-case/CreateUserUseCase"
 import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { FindUserByIdUseCase } from "@/src/domain/user/use-case/FindUserByIdUseCase";
-export const authOptions: AuthOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID || "",
-      clientSecret: process.env.GITHUB_SECRET_ID || "",
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
+import { NextApiRequest, NextApiResponse } from "next";
+import cookie from "cookie";
+export const nexAuthOptions = (
+  req: NextApiRequest,
+  res: NextApiResponse
+): AuthOptions => {
+  return {
+    providers: [
+      GithubProvider({
+        clientId: process.env.GITHUB_CLIENT_ID || "",
+        clientSecret: process.env.GITHUB_SECRET_ID || "",
+      }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
 
-  callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      const isUserExist = await DIContainer.resolve(
-        FindUserByIdUseCase
-      ).execute(user.id);
+    callbacks: {
+      async signIn({ user }) {
+        const isUserExist = await DIContainer.resolve(
+          FindUserByIdUseCase
+        ).execute(user.id);
 
-      if ("error" in isUserExist) {
-        await DIContainer.resolve(CreateUserUseCase).execute({
-          id: user.id,
-          authenticated_method: EnumUserAuthenticatedMethod.GITHUB,
-          email: user.email ?? "",
-          name: user.name ?? "",
-        });
-      }
-      return true;
+        if ("error" in isUserExist) {
+          await DIContainer.resolve(CreateUserUseCase).execute({
+            id: user.id,
+            authenticated_method: EnumUserAuthenticatedMethod.GITHUB,
+            email: user.email ?? "",
+            name: user.name ?? "",
+          });
+        }
+
+        res.setHeader(
+          "Set-Cookie",
+          cookie.serialize("user_type", EnumUserAuthenticatedMethod.GITHUB, {
+            httpOnly: true,
+            secure: true,
+            maxAge: 60 * 60 * 24,
+            sameSite: "lax",
+            path: "/",
+          })
+        );
+
+        return true;
+      },
+      async redirect({ baseUrl }) {
+        return baseUrl + "/lobby";
+      },
     },
-    async redirect({ baseUrl }) {
-      return baseUrl + "/lobby";
-    },
-  },
+  };
 };
-export default NextAuth(authOptions);
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  return NextAuth(req, res, nexAuthOptions(req, res));
+};
