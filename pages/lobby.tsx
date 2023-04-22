103cdc55fa355756da691a06884a9af43a9772da
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2023-04-22 09:01:47 -0500

    feat: add validation to show create room button for github users

--- pages/lobby.tsx
+++ pages/lobby.tsx
@@ -1,6 +1,18 @@
+import { EnumUserAuthenticatedMethod } from "@/src/domain/user/enum/user-authenticated-method.enum";
+import { getCookie } from "cookies-next";
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from "next";
 import Head from "next/head";
 
-export default function LobbyPage() {
+type Data = {
+  userTypeCookie: string;
+};
+export default function LobbyPage({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
       <Head>
@@ -10,7 +22,24 @@ export default function LobbyPage() {
       </Head>
       <main>
         <h1>Lobby Page</h1>
+        {data.userTypeCookie === EnumUserAuthenticatedMethod.GITHUB && (
+          <button> Create chat room </button>
+        )}
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<{ data: Data }> = async (
+  context: GetServerSidePropsContext
+) => {
+  const userTypeCookie = getCookie("user_type", {
+    req: context.req,
+    res: context.res,
+  });
+  return {
+    props: {
+      data: { userTypeCookie: userTypeCookie?.toString() ?? "" },
+    },
+  };
+};
