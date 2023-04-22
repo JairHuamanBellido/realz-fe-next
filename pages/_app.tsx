9e12abac699d3f911b21af5ae41b5674a7e99ca9
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2023-04-22 07:28:29 -0500

    feat: add authentication for guest user use case

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,20 @@ import "uno.css";
 import "reflect-metadata";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   );
 }
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,35 @@
+import useCreateGuestUser from "@/src/core/application/auth/hooks/useCreateGuestUser";
+import { EnumUserAuthenticatedMethod } from "@/src/domain/user/enum/user-authenticated-method.enum";
 import { signIn } from "next-auth/react";
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import { FormEvent } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { useRouter } from "next/router";
 
 export default function AuthPage() {
+  const [username, setUsername] = useState<string>("");
+
+  const { mutate, isSuccess } = useCreateGuestUser();
+  const { replace } = useRouter();
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const id = uuidv4();
+
+    mutate({
+      id: id,
+      email: id + "@example.com",
+      authenticated_method: EnumUserAuthenticatedMethod.GUEST,
+      name: username,
+    });
+  };
+
+  useEffect(() => {
+    if (isSuccess) {
+      replace("/lobby");
+    }
+  }, [isSuccess]);
+
   return (
     <>
       <Head>
@@ -11,7 +39,17 @@ export default function AuthPage() {
       </Head>
       <main>
         <h1>Authentication</h1>
-
+        <form onSubmit={onSubmit}>
+          <input
+            name="text"
+            type="text"
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+          />
+          <button type="submit">Submit</button>
+        </form>
         <button onClick={() => signIn("github")}>Sign in with Github</button>
       </main>
     </>
